f9d098da9618c26b890d562cf83bc55efb4c8e79
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-08-11 05:16:06 -0300

    heroku bug

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/front-pruebaci'));
+app.use(express.static(__dirname + '/dist/clon-homeandcheck'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/front-pruebaci/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/clon-homeandcheck/index.html'));
 });
 
 // default Heroku port
