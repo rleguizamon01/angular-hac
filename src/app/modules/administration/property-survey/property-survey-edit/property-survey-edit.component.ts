78d24e7f190852524d433728c0645be6d52b625f
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-08-11 19:19:08 -0300

    edit component updated

--- src/app/modules/administration/property-survey/property-survey-edit/property-survey-edit.component.ts
+++ src/app/modules/administration/property-survey/property-survey-edit/property-survey-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
+import { PropertySurvey } from 'src/app/interfaces/property-survey';
 
 @Component({
   selector: 'app-property-survey-edit',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertySurveyEditComponent implements OnInit {
 
-  constructor() { }
+  propertySurvey!: PropertySurvey;
+  loading: boolean = true;
+
+  constructor(
+    private api: ApiService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const params = this.route.snapshot.paramMap;
+    const propertySurveyId = Number(params.get('id'));
+
+    this.getPropertySurvey(propertySurveyId);
+    this.loading = false;
+  }
+
+  getPropertySurvey(id: number){
+    this.api.get<PropertySurvey>('propertySurveys/' + id, {})
+    .subscribe(propertySurvey => {
+      this.propertySurvey = propertySurvey;
+      console.log(this.propertySurvey)
+    }); 
   }
 
 }
