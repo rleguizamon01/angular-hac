485f7ee36ca4135b4e9912bc936dfb4104da3427
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-08-18 14:58:09 -0300

    environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUri: 'https://back-hac.herokuapp.com/api/',
+  client_secret: 'jMovvm5Z3RxgY11GkQug5zvcHcmsRmhDyzuBryWr',
 };
